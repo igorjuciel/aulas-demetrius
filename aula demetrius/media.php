fdb0de879de19ffc168cf366ed7eda3c16801dd8
igorjuciel <128391425+igorjuciel@users.noreply.github.com>
2023-04-04 11:49:19 -0300

    Add files via upload

---
+++ aula demetrius/media.php
@@ -0,0 +1,16 @@
+<?php
+$nota1 = $_POST['nota1'];
+$nota2 = $_POST['nota2'];
+$nota3 = $_POST['nota3'];
+
+$media = ($nota1 + $nota2 + $nota3);
+    echo $media."<br>";
+if($media>=7){
+  echo "aprovado";}
+else if (($media<7) &&  ($media>=4)){
+   echo "prova final";}
+else 
+   echo"reprovado";  
+
+
+?>
