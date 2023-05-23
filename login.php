dd4f43d93920b5e64208e7aa9fcd4490837a832b
igorjuciel <128391425+igorjuciel@users.noreply.github.com>
2023-05-23 11:36:42 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form id="login" action = "logado.php"
+ method="POST">
+ <br> <center> login:<input type="text" name="login"><br>
+ <br> <center> senha:<input type= "password" name= "senha"><br>
+
+ <br> <center> <input type="submit" name="entrar" value= "entrar">
+
+</form> 
+</body>
+</html>
+
+
+
