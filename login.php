62b31008cd6c7d6528b04d382686675ef5ffea9f
igorjuciel <128391425+igorjuciel@users.noreply.github.com>
2023-05-25 11:38:37 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Projeto A3 - Programação e Modelagem Manhã</title>
 </head>
 <body>
-<form id="login" action = "logado.php"
- method="POST">
- <br> <center> login:<input type="text" name="login"><br>
- <br> <center> senha:<input type= "password" name= "senha"><br>
-
- <br> <center> <input type="submit" name="entrar" value= "entrar">
-
-</form> 
+	<center>
+		<h1>Login</h1>
+		<form id="form-login" action="logado.php" method="POST">
+			Login: <input type="text" name="login" required><br>
+			Senha: <input type="password" name="senha" required><br><br>
+			<input type="submit" name="entrar" value="Entrar">
+		</form>
+	</center>
 </body>
 </html>
 
