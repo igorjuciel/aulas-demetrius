1b0a264b6f2e4cb0339b69f8eb9e439c0b57bff8
igorjuciel <128391425+igorjuciel@users.noreply.github.com>
2023-05-25 11:38:57 -0300

    Update logado.php

--- logado.php
+++ logado.php
@@ -1,13 +1,20 @@
 <?php
+
 include('conexao.php');
 
-$login = isset ($_POST['login'])?$_POST['login'] : '';
-$senha = isset ($_POST['senha'])?$_POST['senha'] : '';
+$login = isset($_POST['login']) ? $_POST['login'] : '';
+$senha = isset($_POST['senha']) ? $_POST['senha'] : '';
+$select = "select nome, login, senha from login where login = '$login' and senha = '$senha'";
+$query=mysqli_query($conexao, $select);
+$dados=mysqli_fetch_row ($query);
+session_start();
+$_SESSION['nome'] = $dados [0];
 
-if($login == "aluno" && $senha == "fpb") {
-    echo "dados corretos";
-
-}else{
-  echo "dados errados";
+if ($login == $dados[1] && $senha == $dados[2]){
+   header ('location:index.php');
+}else { 
+   header ('location:login.php');
 }
-?>      
+
+
+?>     
