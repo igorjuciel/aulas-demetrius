a45be1786f755a337ffed22b2fec1d86beffd792
igorjuciel <128391425+igorjuciel@users.noreply.github.com>
2023-05-30 11:30:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
-    session_start();
-    echo $_SESSION['nome'];
+  session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<center>    
+olá,
+<?php
+if (isset($_SESSION['nome'])== null){
+?>
+  visitante<br>
+  <a href="login.php">login</a><br>
+<?php } else {
+    echo $_SESSION['nome'];?>   
+<br><a href = "cadastro.php">Cadastrar</a><br>
+<a href = "sair.php">sair</a>   
+<?php } ?>
+</center>
+</body>
+</html>
