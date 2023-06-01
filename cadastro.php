426778bb8320a044bba709bed59469f5122b1076
igorjuciel <128391425+igorjuciel@users.noreply.github.com>
2023-06-01 10:50:34 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>cadastro</h1>
+<form id ="cadastro" action= "cadastrar.php" method= "POST">
+nome: <input type ="text" name="nome" required><br>
+login: <input type ="text" name="login" required><br>
+senha: <input type ="password" name="senha" required><br>
+        <input type ="submit" name="cadastrar" value="cadastrar"required><br><br>
+
+
+
+
+</body>
+</html>
