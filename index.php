62562231d67b18ffa87548fdf984d242209d018c
igorjuciel <128391425+igorjuciel@users.noreply.github.com>
2023-06-06 09:56:25 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" type = "text/css" href = "estilo.css">
     <title>Document</title>
 </head>
 <body>
@@ -25,3 +26,4 @@ if (isset($_SESSION['nome'])== null){
 </center>
 </body>
 </html>
+
